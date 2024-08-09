88b5ecb190511599921d3903d6062276a55a00bd
Korvo-iam <emilaubakirov@gmail.com>
2024-08-09 16:10:35 +0500

    1610090824

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,3 +1,15 @@
 def reverse(string):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Korvo')
+    'ovroK'
+    """
+
     return string[::-1]
-print(reverse('simia_planedo'))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
